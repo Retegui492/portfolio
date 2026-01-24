ca76bd3a1363d9238980d700e687878b44188cea
Jhonatan Hermenegildo Ribeiro <ejhonatan29@gmail.com>
2026-01-23 21:19:11 -0300

    Ajustes visuais e melhorias mobile

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,17 +1,38 @@
-import './navbar.css'
+import { useState } from "react";
+import "./navbar.css";
 
-function Navbar() {
+export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav>
-      <h1 className="logo">Jhonatan Ribeiro</h1>
-      <ul>
+      <div className="logo">Jhonatan</div>
+
+      {/* MENU DESKTOP */}
+      <ul className="nav-links">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">Sobre</a></li>
         <li><a href="#projects">Projetos</a></li>
         <li><a href="#contact">Contato</a></li>
       </ul>
-    </nav>
-  )
-}
 
-export default Navbar
+      {/* BOTÃO HAMBÚRGUER (MOBILE) */}
+      <div
+        className={`hamburger ${menuOpen ? "active" : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* MENU MOBILE */}
+      <div className={`mobile-menu ${menuOpen ? "open" : ""}`}>
+        <a onClick={() => setMenuOpen(false)} href="#home">Home</a>
+        <a onClick={() => setMenuOpen(false)} href="#about">Sobre</a>
+        <a onClick={() => setMenuOpen(false)} href="#projects">Projetos</a>
+        <a onClick={() => setMenuOpen(false)} href="#contact">Contato</a>
+      </div>
+    </nav>
+  );
+}
